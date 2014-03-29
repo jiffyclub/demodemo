61aed49a9e75769868e57c831936f220358c6c0d
Matt Davis <jiffyclub@gmail.com>
2014-03-28 21:57:12 -0700

    Preparing for an API experiment.

---
+++ setup.fish
@@ -0,0 +1,21 @@
+#!/usr/bin/env fish
+
+touch root1.txt
+touch root2.txt
+mkdir -p dir1
+touch dir1-1.txt
+touch dir1-2.txt
+mkdir -p dir1/dir2
+touch dir1/dir2/dir2-1.txt
+touch dir1/dir2/dir2-2.txt
+mkdir -p dir1/dir2/dir3
+touch dir1/dir2/dir3-1.txt
+touch dir1/dir2/dir3-2.txt
+mkdir -p dir4
+touch dir4/dir4-1.txt
+mkdir -p dir5
+touch dir5/dir5-1.txt
+touch dir5/dir5-2.txt
+mkdir -p dir8
+touch dir8/dir8-1.txt
+touch dir8/dir8-2.txt
