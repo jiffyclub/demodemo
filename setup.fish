3bebe11bcefbbdf11ecb6873bf9f058e44e2728e
Matt Davis <jiffyclub@gmail.com>
2014-03-30 09:02:17 -0700

    Move things to their proper place for API experiment.

--- setup.fish
+++ setup.fish
@@ -9,8 +9,8 @@ mkdir -p dir1/dir2
 touch dir1/dir2/dir2-1.txt
 touch dir1/dir2/dir2-2.txt
 mkdir -p dir1/dir2/dir3
-touch dir1/dir2/dir3-1.txt
-touch dir1/dir2/dir3-2.txt
+touch dir1/dir2/dir3/dir3-1.txt
+touch dir1/dir2/dir3/dir3-2.txt
 mkdir -p dir4
 touch dir4/dir4-1.txt
 mkdir -p dir5
